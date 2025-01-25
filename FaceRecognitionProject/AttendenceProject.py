114df1e9667097e562aae6399c667a03b9e6658b
rehmanasad <asadrehmaan7@gmail.com>
2025-01-26 02:25:14 +0500

    facial recognition app using firebase and python

---
+++ FaceRecognitionProject/AttendenceProject.py
@@ -0,0 +1,63 @@
+import cv2
+import face_recognition
+import numpy as np
+
+# Load reference images and learn how to recognize them.
+def load_known_faces(known_image_paths):
+    known_face_encodings = []
+    known_face_names = []
+    for image_path in known_image_paths:
+        image = face_recognition.load_image_file(image_path)
+        encoding = face_recognition.face_encodings(image)[0]
+        known_face_encodings.append(encoding)
+        known_face_names.append(image_path.split('\\')[-1].split('.')[0])  # Assumes the file name is the person's name
+    return known_face_encodings, known_face_names
+
+def main():
+    known_image_paths = ["ImagesBasic/Bill Gates.jpeg", "ImagesBasic/Elon Musk.jpeg","ImagesBasic/Haris.jpeg"]
+    known_face_encodings, known_face_names = load_known_faces(known_image_paths)
+
+    # Initialize the video capture object.
+    video_capture = cv2.VideoCapture(0)
+
+    while True:
+        # Grab a single frame of video
+        ret, frame = video_capture.read()
+
+        # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
+        rgb_frame = frame[:, :, ::-1]
+
+        # Find all the faces and face encodings in the current frame of video
+        face_locations = face_recognition.face_locations(rgb_frame)
+        face_encodings = face_recognition.face_encodings(rgb_frame, face_locations)
+
+        # Loop through each face found in the frame to check if it's someone we know.
+        for (top, right, bottom, left), face_encoding in zip(face_locations, face_encodings):
+            matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
+            name = "Unknown"
+
+            # Use the known face with the smallest distance to the new face
+            face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
+            best_match_index = np.argmin(face_distances)
+            if matches[best_match_index]:
+                name = known_face_names[best_match_index]
+
+            # Draw a box around the face and label the face
+            cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+            cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
+            font = cv2.FONT_HERSHEY_DUPLEX
+            cv2.putText(frame, name, (left + 6, bottom - 6), font, 0.5, (255, 255, 255), 1)
+
+        # Display the resulting image
+        cv2.imshow('Video', frame)
+
+        # Hit 'q' on the keyboard to quit!
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    # Release handle to the webcam
+    video_capture.release()
+    cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    main()
